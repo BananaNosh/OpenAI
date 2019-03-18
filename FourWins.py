85da9a482c6825b3bd27125690ccae1e1bb93113
Jonathan Loos <loos.jonathan.martin@gmail.com>
2019-03-18 23:25:12 +0100

    Remove multiple players Add test_game after training

--- FourWins.py
+++ FourWins.py
@@ -1,43 +1,38 @@
 import gym
-import four_wins_env
-import tensorflow as tf
 import numpy as np
+from four_wins_env.four_wins import COLUMN_COUNT
 
 
-env = gym.make('FourWins-v0')
-
-
-def play(both=False, human=False):
-    env.play_both = both
+def play(env, ai_plays=None):
     total_episodes = 1000
     total_results = np.array([0, 0, 0])
     for i_episode in range(total_episodes):
         observation = env.reset()
         env.render()
         done = False
-        reward = 0
         while not done:
             if observation[1] == 0:
-                action = env.action_space.sample()
-            elif human:
+                if ai_plays is not None:
+                    availability = np.array([1 if env.action_space.contains(i) else 0 for i in range(COLUMN_COUNT)])
+                    action = ai_plays(observation[0], availability)
+                else:
+                    action = env.action_space.sample()
+            else:
                 print("human's turn:")
                 while True:
                     try:
                         action = int(input("human's turn - Which column? "))
                         action -= 1
-                        if 0 <= action <= 6:
+                        if 0 <= action < COLUMN_COUNT:
                             break
                         else:
                             print("Please enter an integer between 1 and 7")
                     except ValueError:
                         print("Please enter an integer between 1 and 7")
-            else:
-                raise AssertionError("Not yet implemented")
             observation, reward, done, info = env.step(action)
             # print(observation)
             env.render()
-            print("rew", reward)
-            if human and done:
+            if done:
                 print(f"You {'draw' if reward == 0 else 'won' if observation[1] == 1 else 'lost'}!")
                 stop = input("Continue? Empty for stop:")
                 if len(stop) == 0:
@@ -47,4 +42,6 @@ def play(both=False, human=False):
 
 
 if __name__ == '__main__':
-    play(True, True)
+    env = gym.make('FourWins-v0')
+    env.play_both = True
+    play(env)
--- Training.py
+++ Training.py
@@ -1,9 +1,10 @@
 # noinspection PyPackageRequirements
-import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
 import gym
-import four_wins_env
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+from FourWins import play
 
 
 def feed_forward_layer(x, target_size, activation_function=None):
@@ -33,16 +34,14 @@ def discount_rewards(rewards, _discount_factor):
 
 
 class GymTraining:
-    def __init__(self, env, observation_dim, hidden_layer_size, action_count, player_count=1,
+    def __init__(self, env, observation_dim, hidden_layer_size, action_count,
                  observation_from_state=None, get_player_number=None):
         self.env = env
         self.input_layer_size = observation_dim
         self.hidden_layer_size = hidden_layer_size
         self.read_out_layer_size = action_count if action_count > 2 else 1
         self.action_count = action_count
-        self.player_count = player_count
         self.observation_from_state = observation_from_state if observation_from_state else lambda x: x
-        self.get_player_number = get_player_number if get_player_number else lambda x: 0
 
     def build_graph(self, decay_rate, decay_steps, initial_learning_rate, variable_scope=None):
         availability = tf.placeholder(tf.float32, [self.action_count])
@@ -61,11 +60,8 @@ class GymTraining:
         with tf.variable_scope("action_selection"):
             gated = tf.multiply(action_probabilities, availability)
             gated = tf.divide(gated, tf.reduce_sum(gated))
-            maximum = tf.reduce_min(tf.boolean_mask(gated, tf.greater(gated, 0)), axis=0)
             gated = tf.clip_by_value(gated, 1*10**(-30), 1)
             log_probabilities = tf.log(gated)
-            # log_availability = tf.log(tf.clip_by_value(availability+maximum/1000000, 0, 1))
-            # gated_probs = tf.divide(gated, tf.reduce_sum(gated))
             act = tf.multinomial(log_probabilities, num_samples=1)[0][0]
             random_action = tf.multinomial([(availability-1)*10000], num_samples=1)[0][0] # sample some random action from all available actions (uniformly)
             decaying_prob_random = tf.train.exponential_decay(0.5, global_step, decay_steps, decay_rate) # the probability to pick the random action must be decaying
@@ -74,7 +70,6 @@ class GymTraining:
             action = tf.cond(random_number > decaying_prob_random, lambda: act, lambda: random_action) # take best action with certain probability, else take random action
             # action = act if np.random.rand()>tf.to_float(decaying_prob_random) else random_action
             log_probability = log_probabilities[:, tf.to_int32(action)]
-            # gated_probability = gated_probs[:, tf.to_int32(action)]
 
         with tf.variable_scope("trainable_variables"):
             trainable_variables = tf.trainable_variables()
@@ -99,57 +94,27 @@ class GymTraining:
         return action, gradient_placeholders, gradients, observation, availability, training_step, log_probabilities, gated, action_probabilities, log_probability, out_layer_weights, out_layer_biases, out_layer_activation
 
     def train(self, training_episodes=40, batch_size=10, discount_factor=0.99, initial_learning_rate=0.05,
-              decay_steps=100, decay_rate=0.75, render=False, save_path=None):
+              decay_steps=100, decay_rate=0.75, render=False, save_path=None, test_play_function=None):
 
         tf.reset_default_graph()
 
-        # build a network for every player
-        action = []
-        gradient_placeholders = []
-        gradients = []
-        observation = []
-        availabilities = []
-        training_step = []
-        log_probs = []
-        gated = []
-        action_probs = []
-        log_probability = []
-        out_layer_weights = []
-        out_layer_biases = []
-        out_layer_activations = []
-        for i in range(self.player_count):
-            scope = f"player_{i}"
-            with tf.variable_scope(scope):
-                _action, _gradient_placeholders, _gradients, _observation, _availability, _training_step, _log_probabilities, _gated, _action_probabilities, _log_prob, _out_layer_weights, _out_layer_biases, _out_layer_activations \
-                    = self.build_graph(decay_rate, decay_steps, initial_learning_rate, variable_scope=scope)
-                action.append(_action)
-                gradient_placeholders.append(_gradient_placeholders)
-                gradients.append(_gradients)
-                observation.append(_observation)
-                availabilities.append(_availability)
-                training_step.append(_training_step)
-                log_probs.append(_log_probabilities)
-                gated.append(_gated)
-                action_probs.append(_action_probabilities)
-                log_probability.append(_log_prob)
-                out_layer_weights.append(_out_layer_weights)
-                out_layer_biases.append(_out_layer_biases)
-                out_layer_activations.append(_out_layer_activations)
+        action, gradient_placeholders, gradients, observation, availability, training_step, log_probabilities, gated, action_probabilities, log_prob, out_layer_weights, out_layer_biases, out_layer_activations \
+            = self.build_graph(decay_rate, decay_steps, initial_learning_rate)
 
-        rewards_cache = [[] for _ in range(self.player_count)]
-        mean_gradients_cache = [[] for _ in range(self.player_count)]
+        rewards_cache = []
+        mean_gradients_cache = []
 
         with tf.Session() as session:
             session.run(tf.global_variables_initializer())
 
             for episode in range(training_episodes):
                 print("episode:", episode)
-                mean_gradients = [[] for _ in range(self.player_count)]
+                mean_gradients = []
 
                 for batch in range(batch_size):
                     done = False
-                    rewards = [[] for _ in range(self.player_count)]
-                    sampled_gradients = [[] for _ in range(self.player_count)]
+                    rewards = []
+                    sampled_gradients = []
 
                     game_state = self.env.reset()
                     print("New game started")
@@ -161,14 +126,13 @@ class GymTraining:
                         _availability = np.array([1 if self.env.action_space.contains(i) else 0
                                                   for i in range(self.action_count)])
                         print("availability", _availability)
-                        current_player = self.get_player_number(game_state)
                         sampled_action, sampled_gradient, _log_probabilities, _gated, _action_probabilities, _log_probability, _weights, _biases, _out_activations, _gradients = session.run(
-                            [action[current_player], gradients[current_player],
-                             log_probs[current_player], gated[current_player], action_probs[current_player], log_probability[current_player],
-                             out_layer_weights[current_player], out_layer_biases[current_player], out_layer_activations[current_player], gradients[current_player]],
+                            [action, gradients,
+                             log_probabilities, gated, action_probabilities, log_prob,
+                             out_layer_weights, out_layer_biases, out_layer_activations, gradients],
                             feed_dict={
-                                observation[current_player]: _observation,
-                                availabilities[current_player]: _availability
+                                observation: _observation,
+                                availability: _availability
                             })
                         # print("gradients", _gradients)
                         print("act probs", _action_probabilities)
@@ -179,40 +143,44 @@ class GymTraining:
                         # print("weights", _weights)
                         # print("biases", _biases)
                         # print("out_activations", _out_activations)
-                        sampled_gradients[current_player].append(sampled_gradient)
+                        sampled_gradients.append(sampled_gradient)
 
                         game_state, reward, done, _ = self.env.step(sampled_action)
                         # if render and ((episode % 10 == 0 and batch == 0) or episode == training_episodes - 1):
                         #     self.env.render()
-                        rewards[current_player].append(reward)
-                        if done:
-                            for i in range(self.player_count):
-                                if i != current_player:
-                                    rewards[i][-1] -= reward
+                        rewards.append(reward)
                     if render:
                         self.env.render()
-                    for i in range(self.player_count):
-                        rewards_cache[i].append(np.sum(rewards[i]))
+                    rewards_cache.append(np.sum(rewards))
 
-                        _rewards = discount_rewards(rewards[i], discount_factor)
-                        rewards[i] = ((_rewards - np.mean(_rewards)) / np.std(_rewards)) \
-                            if not len(np.unique(_rewards)) == 1 else np.zeros_like(_rewards)
+                    _rewards = discount_rewards(rewards, discount_factor)
+                    rewards = ((_rewards - np.mean(_rewards)) / np.std(_rewards)) \
+                        if not len(np.unique(_rewards)) == 1 else np.zeros_like(_rewards)
 
-                        # Scale gradients by normalized discounted reward
-                        scaled_gradients = np.reshape(rewards[i], [-1, 1]) * np.array(sampled_gradients[i])
+                    # Scale gradients by normalized discounted reward
+                    scaled_gradients = np.reshape(rewards, [-1, 1]) * np.array(sampled_gradients)
 
-                        # Calculate mean gradient
-                        mean_gradients[i].append(np.sum(scaled_gradients, axis=0))
+                    # Calculate mean gradient
+                    mean_gradients.append(np.sum(scaled_gradients, axis=0))
 
-                        mean_gradients_cache[i].append(mean_gradients[i])
+                    mean_gradients_cache.append(mean_gradients)
 
-                        for mean_gradient in mean_gradients[i]:
-                            feed_dict = {placeholder: mean_gradient[j]
-                                         for j, placeholder in enumerate(gradient_placeholders[i])}
-                            session.run(training_step[i], feed_dict=feed_dict)
+                    for mean_gradient in mean_gradients:
+                        feed_dict = {placeholder: mean_gradient[j]
+                                     for j, placeholder in enumerate(gradient_placeholders)}
+                        session.run(training_step, feed_dict=feed_dict)
             if save_path:
                 saved = tf.train.Saver().save(session, save_path)
                 print("Model saved in path: %s" % saved)
+            if test_play_function is not None:
+                ai_plays = lambda _observation, _availability: session.run(
+                            [action],
+                            feed_dict={
+                                observation: np.reshape(_observation, [1, -1]),
+                                availability: _availability
+                            })[0]
+                test_play_function(self.env, ai_plays)
+
         self.env.close()
 
         print(rewards_cache)
@@ -223,11 +191,10 @@ class GymTraining:
         axes[0, 0].set_ylabel("Reward")
 
         colors = ["b", "r", "g", "y"]
-        for i in range(self.player_count):
-            color = colors[i % len(colors)]
-            axes[0, 0].plot(np.mean(np.array(rewards_cache[i]).reshape(-1, batch_size), axis=1), color=color)
-            axes[0, 1].plot(rewards_cache[i], color=color)
-            axes[1, 0].plot(np.sum(np.array(rewards_cache[i]).reshape(-1, batch_size) > 0, axis=1), color=color)
+        color = colors[0 % len(colors)]
+        axes[0, 0].plot(np.mean(np.array(rewards_cache).reshape(-1, batch_size), axis=1), color=color)
+        axes[0, 1].plot(rewards_cache, color=color)
+        axes[1, 0].plot(np.sum(np.array(rewards_cache).reshape(-1, batch_size) > 0, axis=1), color=color)
         plt.show()
 
 
@@ -236,13 +203,6 @@ if __name__ == '__main__':
     # training.train(save_path="./saved_models/cart_pole_model.ckpt", training_episodes=40)
     env = gym.make("FourWins-v0")
     env.play_both = True
-    training = GymTraining(env, 42, 84, 7, player_count=1, observation_from_state=lambda x: x[0])
-    training.train(training_episodes=20, batch_size=10, discount_factor=0.99, initial_learning_rate=0.005,
-                   save_path="./saved_models/four_wins_model_one_player.ckpt", render=True)
-    # number = 1
-    # while True:
-    #     loga = np.log(number)
-    #     print(number, loga)
-    #     number /= 10
-    #     if number == 0:
-    #         break
+    training = GymTraining(env, 42, 84, 7, observation_from_state=lambda x: x[0])
+    training.train(training_episodes=200, batch_size=10, discount_factor=0.99, initial_learning_rate=0.005,
+                   save_path="./saved_models/four_wins_model_one_player.ckpt", render=True, test_play_function=play)
